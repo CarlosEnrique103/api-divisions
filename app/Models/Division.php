1d432df6e5e3574c63029eb92cd19b10327db5c0
juancarlos <juancarlosenrique103@gmail.com>
2021-05-16 16:27:40 -0500

    Added relationships for Division model

--- app/Models/Division.php
+++ app/Models/Division.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Division extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'collaborators', 'name_ambassador' 'parent_id'];
+    public function parent()
+    {
+        return $this->belongsTo(Division::class);
+    }
+
+    public function divisions()
+    {
+        return $this->hasMany(Division::class, 'parent_id');
+    }
 }
